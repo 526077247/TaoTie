4d177683d3c5884fe7007dd30e2c40fc77b76617
Tailor <526077247@qq.com>
2025-02-26 14:57:59 +0800

    1.iOS18系统字体获取bug修复。2.代码优化

--- Assets/Scripts/Mono/Define.cs
+++ Assets/Scripts/Mono/Define.cs
@@ -1,4 +1,5 @@
-﻿namespace TaoTie
+﻿
+namespace TaoTie
 {
     public static class Define
     { 
@@ -8,8 +9,13 @@
         public static bool IsSH;
         
         public const string HotfixLoadDir = "Code/Hotfix/";
+#if UNITY_ANDROID
+        public const string AOTLoadDir = "Code/AOTAndroid/";
+#elif UNITY_IOS
+        public const string AOTLoadDir = "Code/AOTiOS/";
+#else //不同BuildTarget的裁剪AOT dll一般不复用。
         public const string AOTLoadDir = "Code/AOT/";
-
+#endif
         public const string HotfixDir = "Assets/AssetsPackage/" + HotfixLoadDir;
         public const string AOTDir = "Assets/AssetsPackage/" + AOTLoadDir;
 #if UNITY_EDITOR
@@ -17,24 +23,35 @@
 #else
         public static readonly bool Debug = UnityEngine.Debug.isDebugBuild;
 #endif
-        public static readonly int DesignScreen_Width = 1920;
-        public static readonly int DesignScreen_Height = 1080;
+        public static readonly int DesignScreenWidth = 1920;
+        public static readonly int DesignScreenHeight = 1080;
         public static int LogLevel = 1;
 
         
 #if FORCE_UPDATE // 是否默认强更 该配置项会影响到有无网络对游戏更新流程的改变
 	    public static bool ForceUpdate = true; //默认强更，无网络或更新失败会只能选退出游戏或重试
 #else
-        public static bool ForceUpdate = false; //默认不强更，无网络或更新失败可以选跳过更新或重试
+        public static bool ForceUpdate = false; //默认不强更，无网络或更新失败可以选跳过或重试
 #endif
-        public static bool Networked =
+            public static bool Networked =
 #if UNITY_EDITOR
-            false;
+                    false;
 #else
         UnityEngine.Application.internetReachability != UnityEngine.NetworkReachability.NotReachable;
 #endif
 
         public static int Process = 1;
-        public static string[] RenameList = {"iOS"};
+
+        /// <summary>
+        /// 0:Json 1:Bytes
+        /// </summary>
+        public static int ConfigType = 
+#if RoslynAnalyzer
+                1;
+#else
+                0;
+#endif
+        
+        public static readonly string[] RenameList = {"iOS"};
     }
 }
